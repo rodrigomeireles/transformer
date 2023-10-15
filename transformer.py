d1929fdbf6dadeb8e6b4273ffe5d5c4f4a42c14d
Rodrigo Meireles de Oliveira <rodrigomeirelesdeoliveira@gmail.com>
2023-10-15 19:45:25 -0300

    Meu primeiro transformer de classificação todo caseiro :)

--- transformer.py
+++ transformer.py
@@ -1,14 +1,15 @@
 import torch
 import torch.nn.functional as F
+from torch import nn
 
 torch.set_default_device("cuda")
 BATCH_SIZE = 1
 SEQ_SIZE = 4
-EMBEDDING_SIZE = 2
-ATTENTION_HEADS = 4
+EMBEDDING_SIZE = 4
+ATTENTION_HEADS = 2
 
 
-class SelfAttention(torch.nn.Module):
+class SelfAttention(nn.Module):
     def __init__(self, k=EMBEDDING_SIZE, heads=ATTENTION_HEADS, mask=False) -> None:
         super().__init__()
         assert (
@@ -16,10 +17,11 @@ class SelfAttention(torch.nn.Module):
         ), "Número de dimensões do embedding não divisível pelo número de cabeças de atenção"
         self.k, self.heads = k, heads
         self.chunksize = self.k // self.heads
-        self.tokeys = torch.nn.Linear(in_features=k, out_features=k, bias=False)
-        self.toqueries = torch.nn.Linear(in_features=k, out_features=k, bias=False)
-        self.tovalues = torch.nn.Linear(in_features=k, out_features=k, bias=False)
-        self.unifyheads = torch.nn.Linear(in_features=k, out_features=k)
+        self.tokeys = nn.Linear(in_features=k, out_features=k, bias=False)
+        self.toqueries = nn.Linear(in_features=k, out_features=k, bias=False)
+        self.tovalues = nn.Linear(in_features=k, out_features=k, bias=False)
+        self.unifyheads = nn.Linear(in_features=k, out_features=k)
+        self.mask = mask
 
     # Ref: https://peterbloem.nl/blog/transformers
     def forward(self, input: torch.Tensor) -> torch.Tensor:
@@ -35,6 +37,10 @@ class SelfAttention(torch.nn.Module):
             )
 
         dot = torch.bmm(kqv_list[1], kqv_list[0].transpose(1, 2)) / (self.k ** (1 / 2))
+        # mask for autoregression
+        if self.mask:
+            indices = torch.triu_indices(seq_size, seq_size, offset=1)
+            dot[:, *indices] = float("-inf")
 
         # calculating self attention
         dot = F.softmax(dot, dim=2)
@@ -44,3 +50,54 @@ class SelfAttention(torch.nn.Module):
         )
         outs = outs.transpose(1, 2).contiguous().view(batch_size, seq_size, self.k)
         return self.unifyheads(outs)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, k, heads) -> None:
+        super().__init__()
+
+        self.attention = SelfAttention(k=k, heads=heads)
+        self.norm1 = nn.LayerNorm(k)
+        self.ff = nn.Sequential(nn.Linear(k, 4 * k), nn.ReLU(), nn.Linear(4 * k, k))
+        self.norm2 = nn.LayerNorm(k)
+
+    # 4 vezes maior porque o tutorial quis
+    def forward(self, x: torch.Tensor):
+        attended = self.attention(x)
+        x = self.norm1(attended + x)
+        fedforward = self.ff(x)
+        return self.norm2(fedforward + x)
+
+
+class ClassifierTransformer(nn.Module):
+    def __init__(
+        self,
+        k: int,
+        heads: int,
+        depth: int,
+        seq_size: int,
+        input_size: int,
+        num_classes: int,
+    ) -> None:
+        super().__init__()
+
+        self.token_emb = nn.Embedding(num_embeddings=input_size, embedding_dim=k)
+        self.pos_emb = nn.Embedding(num_embeddings=seq_size, embedding_dim=k)
+        self.tblocks = nn.Sequential(
+            *[TransformerBlock(k=k, heads=heads) for i in range(depth)]
+        )
+        self.almost_probs = nn.Linear(k, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        :param x: A (b, t) tensor of integer values representing
+                  words (in some predetermined vocabulary).
+        :return: A (b, c) tensor of log-probabilities over the
+                 classes (where c is the nr. of classes).
+        """
+        tokens = self.token_emb(x)
+        b, t, k = tokens.size()
+        positions = self.pos_emb(torch.arange(t))[None, :, :].expand(b, t, k)
+        x = self.tblocks(x + positions)
+        x = self.almost_probs(x.mean(dim=1))
+        return F.log_softmax(x, dim=1)
